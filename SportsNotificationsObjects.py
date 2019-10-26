b77e4847d4d76410f03728a025625f865a673ced
Alex Michael <45311289+alexmichaelb@users.noreply.github.com>
2019-10-26 17:48:22 +0100

    Add files via upload
    Object and Requests File for API

---
+++ SportsNotificationsObjects.py
@@ -0,0 +1,22 @@
+import SportsNotificationsRequests.py
+
+class team:
+  def __init__(teamName, leagueName, standing):
+      self.teamName = teamName
+      self.leagueName = leagueName
+      self.standing = standing
+
+myTeam = team("","","")
+
+class livegame:
+    def __init__(fixture_id, homeTeam, awayTeam, lineups, underWay, halfTimeScore, finalScore):
+        self.fixture_id = fixture_id
+        self.homeTeam = homeTeam
+        self.awayTeam = awayTeam
+        self.lineups = lineups
+        self.underWay = underWay
+        self.halfTimeScore =
+
+liveGame = livegame("","","","","","","","",)
+
+
