cd04590c363c896f97ad137a6e0f48d51a1deda4
Diogo da Silva <diogo.dk.silva@outlook.com>
2019-10-27 00:33:04 +0100

    uodate

--- SportsNotificationsRequests.py
+++ SportsNotificationsRequests.py
@@ -69,4 +69,30 @@ def search_to_file(team_name):
     filewrite = open('testAPI.txt', 'a+').write(str(fatList[0]))
 
 
-search_to_file("Barcelona")
+def get_live_game(teamID):
+    querystring = {"timezone": "Europe/London"}
+    headers = {
+        'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+        'x-rapidapi-key': "c50156e257msh58be36df0d85bfep10de4cjsn14479e1c8f49"
+    }  # must hide
+    ##requests Live fixtures
+    response2 = requests.request("GET", "https://api-football-v1.p.rapidapi.com/v2/fixtures/live/", headers=headers,
+                                 params=querystring)
+
+    ##Saves response in JSON format
+    liveGameData = json.loads(response2.text)
+    liveGameList = liveGameData['api']['fixtures']
+    print(
+        liveGameList)  # prints list of current live games around the world (amazing how many games of professional football there is)
+
+    ##length of list to run for loop counter
+    listLength = len(liveGameList)
+    for x in range(listLength - 1):  ##takes list element, uses dict in a dict to grab team ID comparison
+        y = liveGameList[x]
+        if ((y['homeTeam']['team_id'] == teamID) or (y['awayTeam']['team_id'] == teamID)):
+            ##formats score
+            home_team_name = y['homeTeam']['team_name']
+            home_team_score = y['goalsHomeTeam']
+            away_team_name = y['goalsAwayTeam']
+            away_team_score = y['awayTeam']['team_name']
+            return '{} {} - {} {}'.format(home_team_name, home_team_score, away_team_score, away_team_name)
