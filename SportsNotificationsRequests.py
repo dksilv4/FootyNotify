aca8aee76ae3809ec570a7f087675f0d87831507
marcy <marcelmendes@hotmail.co.uk>
2019-10-27 00:33:04 +0100

    changes to the sports classes. created functions to return to main

--- SportsNotificationsRequests.py
+++ SportsNotificationsRequests.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+'''Requests that use the API to search for various things including the Team details/name/id/ fixtures'''
 
 def search_for_name(team_name):
     url = "https://api-football-v1.p.rapidapi.com/v2/teams/search/" + team_name
@@ -12,7 +12,7 @@ def search_for_name(team_name):
 
     response = requests.request("GET", url, headers=headers)
 
-    data = json.loads(response.text)
+    data = json.loads(response.text) #the data from all matching football teams
 
     fatList = data['api']['teams']
     result = (fatList[0]["name"])
@@ -30,7 +30,7 @@ def search_for_team_id(team_name):
 
     response = requests.request("GET", url, headers=headers)
 
-    data = json.loads(response.text)
+    data = json.loads(response.text) #the data from all matching football teams
 
     fatList = data['api']['teams']
     result = (fatList[0]["team_id"])
@@ -47,7 +47,7 @@ def search_for_team_details(team_name):
 
     response = requests.request("GET", url, headers=headers)
 
-    data = json.loads(response.text)
+    data = json.loads(response.text) #the data from all matching football teams
 
     fatList = data['api']['teams']
     return fatList[0]
@@ -61,12 +61,10 @@ def search_to_file(team_name):
         'x-rapidapi-key': "c50156e257msh58be36df0d85bfep10de4cjsn14479e1c8f49"
     }  # must hide
 
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, headers=headers) #the data from all matching football teams
 
     data = json.loads(response.text)
 
     fatList = data['api']['teams']
     filewrite = open('testAPI.txt', 'a+').write(str(fatList[0]))
 
-
-search_to_file("Barcelona")
