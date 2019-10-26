92b446b1902099586cb7012ddd1cf7ba965747aa
marcy <marcelmendes@hotmail.co.uk>
2019-10-26 21:28:15 +0100

    changes to the sports classes

--- SportsNotificationsRequests.py
+++ SportsNotificationsRequests.py
@@ -8,7 +8,7 @@ url = "https://api-football-v1.p.rapidapi.com/v2/teams/search/"+userteam
 headers = {
     'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
     'x-rapidapi-key': "af10c3e836mshb4fb54db83a539fp154ebajsn4fdd167bfc6c"
-    }
+    } #must hide
 
 response = requests.request("GET", url, headers=headers)
 
