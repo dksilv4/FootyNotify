92b9ed430e08fe71473eff69d6b9318c623e6a36
Alex Michael <45311289+alexmichaelb@users.noreply.github.com>
2019-10-26 23:21:07 +0100

    Add files via upload
    Live score updates can now be requested. Object py file just a placeholder.

--- SportsNotificationsObjects.py
+++ SportsNotificationsObjects.py
@@ -1,26 +1,22 @@
 import SportsNotificationsRequests.py
 
+class team:
+  def __init__(teamName, leagueName, standing):
+      self.teamName = teamName
+      self.leagueName = leagueName
+      self.standing = standing
 
-class Team:
-    def __init__(self, teamName=None, leagueName=None, standing=None):
-        self.teamName = teamName
-        """self.leagueName = leagueName"""
-        self.standing = standing
+myTeam = team("","","")
 
-
-myTeam = Team("", "", "")
-
-
-class LiveGame:
-
-    def __init__(self, fixture_id, homeTeam, awayTeam, lineups, underWay, halfTimeScore, finalScore):
+class livegame:
+    def __init__(fixture_id, homeTeam, awayTeam, lineups, underWay, halfTimeScore, finalScore):
         self.fixture_id = fixture_id
         self.homeTeam = homeTeam
         self.awayTeam = awayTeam
         self.lineups = lineups
         self.underWay = underWay
-        self.halfTimeScore = halfTimeScore
-        self.finalScore = finalScore
+        self.halfTimeScore =
+
+liveGame = livegame("","","","","","","","",)
 
 
-liveGame = LiveGame("", "", "", "", "", "", "", "", )
