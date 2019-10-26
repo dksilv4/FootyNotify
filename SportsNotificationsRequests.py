e6900d455efb4268add0cb24dfcb2bfba1fc49a0
marcy <marcelmendes@hotmail.co.uk>
2019-10-26 23:31:54 +0100

    changes to the sports classes. created functions to return to main

--- SportsNotificationsRequests.py
+++ SportsNotificationsRequests.py
@@ -3,7 +3,6 @@ import json
 
 
 def search_for_name(team_name):
-
     url = "https://api-football-v1.p.rapidapi.com/v2/teams/search/" + team_name
 
     headers = {
@@ -15,7 +14,6 @@ def search_for_name(team_name):
 
     data = json.loads(response.text)
 
-
     fatList = data['api']['teams']
     result = (fatList[0]["name"])
 
@@ -23,12 +21,11 @@ def search_for_name(team_name):
 
 
 def search_for_team_id(team_name):
-
     url = "https://api-football-v1.p.rapidapi.com/v2/teams/search/" + team_name
 
     headers = {
         'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
-        'x-rapidapi-key': "af10c3e836mshb4fb54db83a539fp154ebajsn4fdd167bfc6c"
+        'x-rapidapi-key': "c50156e257msh58be36df0d85bfep10de4cjsn14479e1c8f49"
     }  # must hide
 
     response = requests.request("GET", url, headers=headers)
@@ -39,13 +36,13 @@ def search_for_team_id(team_name):
     result = (fatList[0]["team_id"])
     return result
 
-def search_for_team_details(team_name):
 
+def search_for_team_details(team_name):
     url = "https://api-football-v1.p.rapidapi.com/v2/teams/search/" + team_name
 
     headers = {
         'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
-        'x-rapidapi-key': "af10c3e836mshb4fb54db83a539fp154ebajsn4fdd167bfc6c"
+        'x-rapidapi-key': "c50156e257msh58be36df0d85bfep10de4cjsn14479e1c8f49"
     }  # must hide
 
     response = requests.request("GET", url, headers=headers)
@@ -54,3 +51,22 @@ def search_for_team_details(team_name):
 
     fatList = data['api']['teams']
     return fatList[0]
+
+
+def search_to_file(team_name):
+    url = "https://api-football-v1.p.rapidapi.com/v2/teams/search/" + team_name
+
+    headers = {
+        'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+        'x-rapidapi-key': "c50156e257msh58be36df0d85bfep10de4cjsn14479e1c8f49"
+    }  # must hide
+
+    response = requests.request("GET", url, headers=headers)
+
+    data = json.loads(response.text)
+
+    fatList = data['api']['teams']
+    filewrite = open('testAPI.txt', 'a+').write(str(fatList[0]))
+
+
+search_to_file("Barcelona")
