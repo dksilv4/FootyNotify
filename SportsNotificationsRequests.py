de4c7fe247e8f32475e1669250bc1385adf992d7
Diogo da Silva <diogo.dk.silva@outlook.com>
2019-10-27 01:42:45 +0000

    fixed live game feature

--- SportsNotificationsRequests.py
+++ SportsNotificationsRequests.py
@@ -1,7 +1,10 @@
 import requests
 import json
+
 '''Requests that use the API to search for various things including the Team details/name/id/ fixtures'''
 token = '3e912ca4e7msh3e11bf13a48a111p1e25fdjsnf736c67d588a'
+
+
 def search_for_name(team_name):
     url = "https://api-football-v1.p.rapidapi.com/v2/teams/search/" + team_name
 
@@ -12,7 +15,7 @@ def search_for_name(team_name):
 
     response = requests.request("GET", url, headers=headers)
 
-    data = json.loads(response.text) #the data from all matching football teams
+    data = json.loads(response.text)  # the data from all matching football teams
 
     fatList = data['api']['teams']
     result = (fatList[0]["name"])
@@ -30,7 +33,7 @@ def search_for_team_id(team_name):
 
     response = requests.request("GET", url, headers=headers)
 
-    data = json.loads(response.text) #the data from all matching football teams
+    data = json.loads(response.text)  # the data from all matching football teams
 
     fatList = data['api']['teams']
     result = (fatList[0]["team_id"])
@@ -47,7 +50,7 @@ def search_for_team_details(team_name):
 
     response = requests.request("GET", url, headers=headers)
 
-    data = json.loads(response.text) #the data from all matching football teams
+    data = json.loads(response.text)  # the data from all matching football teams
 
     fatList = data['api']['teams']
     return fatList[0]
@@ -61,7 +64,7 @@ def search_to_file(team_name):
         'x-rapidapi-key': token
     }  # must hide
 
-    response = requests.request("GET", url, headers=headers) #the data from all matching football teams
+    response = requests.request("GET", url, headers=headers)  # the data from all matching football teams
 
     data = json.loads(response.text)
 
@@ -71,29 +74,24 @@ def search_to_file(team_name):
 
 def get_live_game(teamID):
     querystring = {"timezone": "Europe/London"}
-    headers = {
-        'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
-        'x-rapidapi-key': token
-    }  # must hide
-    ##requests Live fixtures
-    response2 = requests.request("GET", "https://api-football-v1.p.rapidapi.com/v2/fixtures/live/", headers=headers,
-                                 params=querystring)
+    headers = {'x-rapidapi-host': "api-football-v1.p.rapidapi.com", 'x-rapidapi-key': token}
+    # requests Live fixtures
+    response = requests.request("GET", "https://api-football-v1.p.rapidapi.com/v2/fixtures/live/", headers=headers,
+                                params=querystring)
+    # Saves response in JSON format
+    live_game_data = json.loads(response.text)
+    live_game_list = live_game_data['api']['fixtures']
 
-    ##Saves response in JSON format
-    liveGameData = json.loads(response2.text)
-    liveGameList = liveGameData['api']['fixtures']
-    print(
-        liveGameList)  # prints list of current live games around the world (amazing how many games of professional football there is)
-
-    ##length of list to run for loop counter
-    listLength = len(liveGameList)
-    for x in range(listLength - 1):  ##takes list element, uses dict in a dict to grab team ID comparison
-        y = liveGameList[x]
-        if ((y['homeTeam']['team_id'] == teamID) or (y['awayTeam']['team_id'] == teamID)):
-            ##formats score
+    for x in range(0, len(live_game_list)):  # takes list element, uses dict in a dict to grab team ID comparison
+        y = live_game_list[x]
+        home_team_id = y['homeTeam']['team_id']
+        away_team_id = y['awayTeam']['team_id']
+        if home_team_id == teamID or away_team_id == teamID:
+            # formats score output
             home_team_name = y['homeTeam']['team_name']
             home_team_score = y['goalsHomeTeam']
             away_team_name = y['goalsAwayTeam']
             away_team_score = y['awayTeam']['team_name']
             return '{} {} - {} {}'.format(home_team_name, home_team_score, away_team_score, away_team_name)
-
+        else:
+            return None
